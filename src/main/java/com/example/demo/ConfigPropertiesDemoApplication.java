7fc8d343dd4454240dbc133189073cc5e56a562e
wuzhuojian <zjwu920@hotmail.com>
2019-11-16 11:01:10 +0900

    sample

--- src/main/java/com/example/demo/ConfigPropertiesDemoApplication.java
+++ src/main/java/com/example/demo/ConfigPropertiesDemoApplication.java
@@ -1,10 +1,9 @@
 package com.example.demo;
 
-        import com.example.demo.config.AppProperties;
-        import org.springframework.boot.SpringApplication;
-        import org.springframework.boot.autoconfigure.SpringBootApplication;
-        import org.springframework.boot.context.properties.EnableConfigurationProperties;
-        import org.springframework.context.annotation.Profile;
+import com.example.demo.config.AppProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AppProperties.class)
--- src/test/java/com/example/demo/ConfigPropertiesDemoApplicationTests.java
+++ src/test/java/com/example/demo/ConfigPropertiesDemoApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 import com.example.demo.config.AppProperties;
 import com.example.demo.model.Security;
-import org.apache.juli.FileHandler;
-import org.junit.*;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
